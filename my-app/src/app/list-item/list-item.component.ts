37d2ec09f0a585c9b7e4f78618d8761a75b44d87
Unknown <christophegueroult@gmail.com>
2017-10-03 10:53:50 +0200

    add component items

---
+++ my-app/src/app/list-item/list-item.component.ts
@@ -0,0 +1,17 @@
+import { AppComponent } from './../app.component';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.scss']
+})
+export class ListItemComponent implements OnInit {
+  collection: any;
+  constructor(private _AppComponent: AppComponent) { }
+
+  ngOnInit() {
+    this.collection = this._AppComponent.collection;
+  }
+
+}
