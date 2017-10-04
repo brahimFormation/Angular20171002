ec4f500253171b90e07e22915b68997f653a0f1e
Unknown <christophegueroult@gmail.com>
2017-10-04 16:58:07 +0200

    Add module for items

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,23 +1,12 @@
-import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { environment } from '../environments/environment.prod';
+import { ItemModuleModule } from './item-module/item-module.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { ListItemComponent } from './list-item/list-item.component';
 import { ModalComponent } from './modal/modal.component';
-import { FormComponent } from './form/form.component';
-import { FilterPipe } from './filter.pipe';
-import { StateDirective } from './state.directive';
-import { CollectionService } from './collection.service';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
@@ -25,31 +14,20 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   declarations: [
     AppComponent,
     MenuComponent,
-    ListItemComponent,
     ModalComponent,
-    FormComponent,
-    FilterPipe,
-    StateDirective,
     HomeComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     NgbModule.forRoot(),
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase, 'my-app'),
-    AngularFireDatabaseModule,
-    AngularFirestoreModule,
-    AngularFireAuthModule
+    ItemModuleModule,
+    AppRoutingModule
   ],
   entryComponents: [
     ModalComponent
   ],
-  providers: [
-    CollectionService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
