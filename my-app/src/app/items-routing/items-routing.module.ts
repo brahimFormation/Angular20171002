6a0e767d69cc09b84f99ec562058c89b89b5071e
Unknown <christophegueroult@gmail.com>
2017-10-04 17:20:59 +0200

    Maj doc

---
+++ my-app/src/app/items-routing/items-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ListItemComponent } from './../item-module/list-item/list-item.component';
+import { FormComponent } from './../item-module/form/form.component';
+
+const itemsRoutes: Routes = [
+  { path: 'list', component: ListItemComponent },
+  { path: 'form', component: FormComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(
+      itemsRoutes
+    )
+  ],
+  declarations: []
+})
+export class ItemsRoutingModule { }
