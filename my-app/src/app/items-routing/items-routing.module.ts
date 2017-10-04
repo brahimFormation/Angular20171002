25d7cd4cf50441d0a3153f48e9fade0f08ec7e83
Unknown <christophegueroult@gmail.com>
2017-10-04 23:50:21 +0200

    Form and Modal reutilisable

--- my-app/src/app/items-routing/items-routing.module.ts
+++ my-app/src/app/items-routing/items-routing.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ListItemComponent } from './../item-module/list-item/list-item.component';
 import { FormComponent } from './../item-module/form/form.component';
+import { AdditemComponent } from '../item-module/additem/additem.component';
 
 const itemsRoutes: Routes = [
   { path: 'list', component: ListItemComponent },
-  { path: 'form', component: FormComponent },
+  { path: 'additem', component: AdditemComponent },
 ];
 
 @NgModule({
