ee975b703825f4452abaf00edaa4985802119424
Unknown <christophegueroult@gmail.com>
2017-10-13 21:51:27 +0200

    add enum for states

--- my-app/src/app/items/components/item/item.component.ts
+++ my-app/src/app/items/components/item/item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CollectionService } from '@app/core';
 
 import { Item } from '../../models/item';
+import { States } from '@app/shared/enums/states.enum';
 
 @Component({
   selector: 'app-item',
@@ -9,14 +10,18 @@ import { Item } from '../../models/item';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
+
   @Input() item: Item;
+  states = States;
+
   constructor(private _CollectionService: CollectionService) { }
 
   ngOnInit() {
+    // console.log(this.states)
   }
 
   changeState(key: string, state: number) {
-    console.log(key)
+    console.log(key);
     this._CollectionService.changeState(key, state);
   }
 }
